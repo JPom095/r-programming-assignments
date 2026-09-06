5b46c38f0846d2472a974e17b575f46fe791fba4
Josiah Pomeroy <josiahpomeroy@usf.edu>
2026-09-06 16:10:38 -0400

    Assignment 2 code update and link to blog post

---
+++ Assignment 2 R.R
@@ -0,0 +1,14 @@
+#Original Function Code
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+myMean <- function(assignment2) {
+  return(sum(assignment) / length(someData))
+}
+
+#Updated Function Code With Correct Variable
+myMean <- function(assignment2) {
+  return(sum(assignment2) / length(assignment2))
+}
+myMean(assignment2)
+
+#https://josiahpomeroyrprogramming.wordpress.com/2026/09/06/assignment-2-mean-function/
+#Please see link to access blog post.
